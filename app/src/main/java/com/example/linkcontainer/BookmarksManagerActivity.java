8821afef051d909dfca897baf16f6062dab10834
lucapetrillo99 <lucapetrillo99@gmail.com>
2021-04-28 11:26:22 +0200

    Added method to import bookmarks

--- app/src/main/java/com/example/linkcontainer/BookmarksManagerActivity.java
+++ app/src/main/java/com/example/linkcontainer/BookmarksManagerActivity.java
@@ -4,23 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.DocumentsContract;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -30,29 +30,38 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import at.blogc.android.views.ExpandableTextView;
 
 public class BookmarksManagerActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE = 10;
     private static final int PERMISSION_REQUEST_STORAGE = 1000;
+    private static final String IMPORTED = "Importati";
+    private static final long ALARM_START_TIME = -1;
     private RelativeLayout importOption;
     private ExpandableTextView importOptionText;
     private TextView description;
     private Button selectButton;
+    private final Set<String> importedBookmarks = new LinkedHashSet<>();
+    private int bookmarksCounter = 0;
+    private DatabaseHandler db;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmarks_manager);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        toolbarTitle.setText("Importa/Esporta");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_back_button);
+        db = DatabaseHandler.getInstance(getApplicationContext());
 
         importOption =  findViewById(R.id.import_option);
         importOptionText = findViewById(R.id.expandableTextView);
@@ -66,7 +75,7 @@ public class BookmarksManagerActivity extends AppCompatActivity {
 
         importOptionTextListener();
         selectButtonListener();
-
+        toolbar.setNavigationOnClickListener(v -> finish());
     }
 
     private void importOptionTextListener() {
@@ -93,28 +102,21 @@ public class BookmarksManagerActivity extends AppCompatActivity {
             } else {
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("text/*");
+                intent.setType("text/html");
 
                 startActivityForResult(intent, PERMISSION_REQUEST_STORAGE);
-
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        Log.i("IOFHDO", "ENTRO RESULT");
-        Log.i("IOFHDO", "CODE MIO" + REQUEST_CODE + " RESULT come dovrebbe " + Activity.RESULT_OK);
-        Log.i("IOFHDO", "CODE " + requestCode + " RESULT " + resultCode);
         if (requestCode == PERMISSION_REQUEST_STORAGE && resultCode == Activity.RESULT_OK) {
-            Log.i("IOFHDO", "ENTRO IF");
-            // The result data contains a URI for the document or directory that
-            // the user selected.
             Uri uri;
-            Log.i("IOFHDO", String.valueOf(data != null));
             if (data != null) {
                 uri = data.getData();
-                readTextFromUri(uri);
+                getBookmarksFromUri(uri);
+                importOptionsDialog();
             }
 
             super.onActivityResult(requestCode, resultCode, data);
@@ -126,11 +128,6 @@ public class BookmarksManagerActivity extends AppCompatActivity {
         if (requestCode == PERMISSION_REQUEST_STORAGE) {
             if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
                 showMessage();
-            } else {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("text/*");
-                startActivityForResult(intent, REQUEST_CODE);
             }
         }
     }
@@ -155,7 +152,7 @@ public class BookmarksManagerActivity extends AppCompatActivity {
         snackbar.show();
     }
 
-    private void readTextFromUri(Uri uri) {
+    private void getBookmarksFromUri(Uri uri) {
         StringBuilder stringBuilder = new StringBuilder();
         try (InputStream inputStream =
                      getContentResolver().openInputStream(uri);
@@ -169,11 +166,85 @@ public class BookmarksManagerActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         Document document = Jsoup.parse(stringBuilder.toString());
-        Elements links = document.select("a[href]");
-        for (Element element : links) {
-            
-        }
 
+        Elements links =  document.select("a[href]");
+
+        for (Element element: links) {
+            importedBookmarks.add(element.attr("href"));
+        }
     }
 
+    private void importOptionsDialog() {
+        String question;
+        if (importedBookmarks.size() > 1) {
+            question = " segnalibri?";
+        } else {
+            question = " segnalibro?";
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(BookmarksManagerActivity.this);
+        builder.setTitle("Sei sicuro di voler importare " + importedBookmarks.size() + question);
+        builder.setPositiveButton("Importa", (dialog, which) -> importBookmarks());
+        builder.setNeutralButton("Annulla", (dialog, which) -> { });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void importBookmarks() {
+        LoadingDialog loadingDialog = new LoadingDialog(BookmarksManagerActivity.this);
+        loadingDialog.startLoading();
+        String queryResult = db.getCategoryId(IMPORTED);
+        String categoryId;
+        if (queryResult == null) {
+            db.addCategory(IMPORTED);
+            categoryId = db.getCategoryId(IMPORTED);
+        } else {
+            categoryId = queryResult;
+        }
+
+        if (importedBookmarks.size() > 0) {
+            Thread thread = new Thread() {
+                public void run() {
+                    for (String link : importedBookmarks) {
+                        try {
+                            Document document = Jsoup.connect(link).get();
+                            if (document != null) {
+                                Bookmark bookmark = new Bookmark();
+                                Elements metaTags = document.getElementsByTag("meta");
+                                for (Element element : metaTags) {
+                                    if (element.attr("property").equals("og:image")) {
+                                        bookmark.setImage(element.attr("content"));
+                                    } else if (element.attr("property").equals("og:site_name")) {
+                                        bookmark.setTitle(element.attr("content"));
+                                    } else if (element.attr("name").equals("description")) {
+                                        bookmark.setDescription(element.attr("content"));
+                                    }
+                                }
+                                bookmark.setLink(link);
+                                bookmark.setCategory(categoryId);
+                                bookmark.setReminder(ALARM_START_TIME);
+
+                                boolean insertionResult = db.addBookmark(bookmark);
+                                if (insertionResult)
+                                    bookmarksCounter++;
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    runOnUiThread(() -> {
+                        loadingDialog.dismissLoading();
+                        Toast.makeText(BookmarksManagerActivity.this, bookmarksCounter +
+                                        " segnalibri importati!",
+                                Toast.LENGTH_LONG).show();
+                    });
+                }
+            };
+            thread.start();
+        } else {
+            Toast.makeText(BookmarksManagerActivity.this, "Non ci sono segnalibri da importare!",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
 }

7ca2ed88588187e6ab31e36f08dcc70c74856c2c
lucapetrillo99 <lucapetrillo99@gmail.com>
2021-08-12 18:32:40 +0200

    Modified image preview animations
    - Modified image preview toolbar

--- app/src/main/java/com/ilpet/yabm/utils/ImagePreview.java
+++ app/src/main/java/com/ilpet/yabm/utils/ImagePreview.java
@@ -1,5 +1,9 @@
 package com.ilpet.yabm.utils;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +13,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.DialogFragment;
 
 import com.ilpet.yabm.R;
@@ -18,7 +24,7 @@ import com.squareup.picasso.Picasso;
 public class ImagePreview extends DialogFragment {
     private static String image;
     private static String title;
-    private boolean tap = false;
+    private boolean tap = true;
 
     public static ImagePreview newInstance(String imageToLoad, String imageTitle) {
         image = imageToLoad;
@@ -26,12 +32,12 @@ public class ImagePreview extends DialogFragment {
         return new ImagePreview();
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setStyle(DialogFragment.STYLE_NO_FRAME, R.style.DialogAnimation);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -39,9 +45,16 @@ public class ImagePreview extends DialogFragment {
         Toolbar toolbar = view.findViewById(R.id.toolbar);
         ImageView imageView = view.findViewById(R.id.preview_image);
         TextView toolbarTitle = view.findViewById(R.id.toolbar_title);
+        toolbar.setBackgroundColor(Color.TRANSPARENT);
         toolbarTitle.setText(title);
+        toolbarTitle.setTextColor(Color.WHITE);
 
-        toolbar.setNavigationIcon(R.drawable.ic_back_button);
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(),
+                R.drawable.ic_back_button, null);
+        assert drawable != null;
+        drawable.setTint(Color.WHITE);
+        toolbar.setNavigationIcon(drawable);
+
         Picasso.get().load(image)
                 .fit()
                 .centerInside()
@@ -59,5 +72,16 @@ public class ImagePreview extends DialogFragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+        }
     }
 }

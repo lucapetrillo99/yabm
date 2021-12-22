fccaea0ec49b6f15a9e45ca1a4e9f5dc86777332
Luca Petrillo <64256273+lucapetrillo99@users.noreply.github.com>
2021-12-22 17:34:29 +0100

    Fixed bookmark insertion
    - Modified the maximum waiting time for retrieving link information
    - Managed bookmark insertion without internet connection

--- app/src/main/java/com/ilpet/yabm/utils/Utils.java
+++ app/src/main/java/com/ilpet/yabm/utils/Utils.java
@@ -13,7 +13,7 @@ public class Utils {
 
         return Observable.fromCallable(() -> {
             try {
-                return Jsoup.connect(url).timeout(0).get();
+                return Jsoup.connect(url).timeout(15000).get();
             } catch (IOException exception) {
                 throw new RuntimeException(exception);
             }
